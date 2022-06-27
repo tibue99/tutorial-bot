3e4dff74213a1b0dbb4b0eb218458ad00676aedc
Timo <tibue69@gmail.com>
2022-06-27 16:51:09 +0200

    Part 2

---
+++ Part 2/main.py
@@ -0,0 +1,27 @@
+import discord
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = discord.Bot(intents=intents)
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} ist online")
+
+
+@bot.event
+async def on_message(msg):
+    if msg.author.bot:
+        return
+
+    await msg.channel.send("Du stinkst")
+
+
+@bot.event
+async def on_message_delete(msg):
+    await msg.channel.send(f"Eine Nachricht von {msg.author} wurde gelöscht: {msg.content}")
+
+
+bot.run("")
