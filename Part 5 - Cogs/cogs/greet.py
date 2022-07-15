2389e12b1bf1d0bfc2327dd1cb60c6c0c8ae234e
Timo <tibue69@gmail.com>
2022-07-15 14:48:08 +0200

    Part 5

---
+++ Part 5 - Cogs/cogs/greet.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from discord.commands import slash_command
+
+
+class Base(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command()
+    async def greet(self, ctx):
+        await ctx.respond(f"Hey {ctx.author.mention}")
+
+    @commands.Cog.listener
+    async def on_member_join(self, member):
+        embed = discord.Embed(
+            title="Willkommen",
+            description=f"Hey {member.mention}",
+            color=discord.Color.orange()
+        )
+
+        channel = await self.bot.fetch_channel(123456789)  # hier channel id einfügen
+        await channel.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Base(bot))
