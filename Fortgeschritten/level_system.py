1acfd4b7471e82f577f687d5a0c4d5f388025139
Timo <tibue69@gmail.com>
2022-10-28 12:22:58 +0200

    Add database tutorial (part 3)

--- Fortgeschritten/level_system.py
+++ Fortgeschritten/level_system.py
@@ -14,12 +14,14 @@ class LevelSystem(commands.Cog):
     @staticmethod
     def get_level(xp):
         lvl = 1
+        amount = 100
 
         while True:
-            xp -= 100
+            xp -= amount
             if xp < 0:
                 return lvl
             lvl += 1
+            amount += 100
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -33,31 +35,51 @@ class LevelSystem(commands.Cog):
                 )"""
             )
 
+    async def check_user(self, user_id):
+        async with aiosqlite.connect(self.DB) as db:
+            await db.execute("INSERT OR IGNORE INTO users (user_id) VALUES (?)", (user_id,))
+            await db.commit()
+
+    async def get_xp(self, user_id):
+        await self.check_user(user_id)
+        async with aiosqlite.connect(self.DB) as db:
+            async with db.execute("SELECT xp FROM users WHERE user_id = ?", (user_id,)) as cursor:
+                result = await cursor.fetchone()
+
+        return result[0]
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author.bot:
             return
+        if not message.guild:
+            return
         xp = random.randint(10, 20)
-        
+
+        await self.check_user(message.author.id)
         async with aiosqlite.connect(self.DB) as db:
-            await db.execute(
-                "INSERT OR IGNORE INTO users (user_id) VALUES (?)", (message.author.id,)
-            )
             await db.execute(
                 "UPDATE users SET msg_count = msg_count + 1, xp = xp + ? WHERE user_id = ?", (xp, message.author.id)
             )
             await db.commit()
 
+        # check level up
+        new_xp = await self.get_xp(message.author.id)
+
+        old_level = self.get_level(new_xp - xp)
+        new_level = self.get_level(new_xp)
+
+        if old_level == new_level:
+            return
+
+        if new_level == 2:
+            role = message.guild.get_role(123456789)  # hier role ID einfügen
+            await message.author.add_roles(role)
+            await message.channel.send(f"Level Up! Du hast die Rolle {role.mention} erhalten!")
+
     @slash_command()
     async def rank(self, ctx):
-        async with aiosqlite.connect(self.DB) as db:
-            async with db.execute("SELECT xp FROM users WHERE user_id = ?", (ctx.author.id,)) as cursor:
-                result = await cursor.fetchone()
-                if result is None:
-                    await ctx.respond("Du bist noch nicht in der Datenbank.", ephemeral=True)
-                    return
-
-        xp = result[0]
+        xp = await self.get_xp(ctx.author.id)
         lvl = self.get_level(xp)
         await ctx.respond(f"Du hast **{xp}** XP und bist Level **{lvl}**")
 
