027e04ce933d09e18c9d41f535a1569f42236ef0
Timo <tibue69@gmail.com>
2022-12-22 21:30:04 +0100

    Add autocomplete tutorial (Part 17)

---
+++ Tutorialreihe/part17_autocomplete.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+from discord.commands import slash_command, Option
+
+food = ["Pizza", "Pommes", "Döner"]
+
+
+def get_food(ctx: discord.AutocompleteContext):
+    if "99" in ctx.interaction.user.display_name:
+        return food + ["Kekse"]
+
+    return food
+
+
+class Base(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @slash_command()
+    async def essen(
+            self, ctx, auswahl: Option(str, autocomplete=get_food)
+    ):
+        await ctx.respond(f"Du hast ✨ **{auswahl}** ✨ gewählt")
+
+
+def setup(bot):
+    bot.add_cog(Base(bot))
