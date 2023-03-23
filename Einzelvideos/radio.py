ca32d7e5cb197c6b790e3e3c535499d8e4d7c731
Timo <tibue69@gmail.com>
2023-03-23 12:34:44 +0100

    Add radio tutorial (Part 2)

--- Einzelvideos/radio.py
+++ Einzelvideos/radio.py
@@ -9,7 +9,19 @@ class Radio(commands.Cog):
 
     @slash_command(description="Starte das Radio")
     async def play(self, ctx):
-        await ctx.author.voice.channel.connect()
+        if ctx.author.voice is None:
+            return await ctx.respond("Du musst erst einem Voice Channel beitreten.")
+
+        if not ctx.author.voice.channel.permissions_for(ctx.guild.me).connect:
+            return await ctx.respond("Ich habe keine Rechte, um deinem Channel beizutreten.")
+
+        if ctx.voice_client is None:
+            await ctx.author.voice.channel.connect()  # Bot ist in keinem Voice Channel
+        else:
+            await ctx.voice_client.move_to(ctx.author.voice.channel)  # Bot ist schon in einem anderen Voice Channel
+
+        if ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
 
         ctx.voice_client.play(
             discord.FFmpegPCMAudio("https://streams.ilovemusic.de/iloveradio1.mp3")
@@ -18,6 +30,9 @@ class Radio(commands.Cog):
 
     @slash_command(description="Stoppe das Radio")
     async def leave(self, ctx):
+        if ctx.voice_client is None:
+            return await ctx.respond("Ich bin mit keinem Voice Channel verbunden.")
+
         await ctx.voice_client.disconnect()
         await ctx.respond("Bis bald")
 
