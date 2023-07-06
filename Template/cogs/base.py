7adf93872ee9216b16d6f1c9c51da05e3b9af275
Timo <tibue69@gmail.com>
2023-07-06 23:42:40 +0200

    Add type hints

--- Template/cogs/base.py
+++ Template/cogs/base.py
@@ -4,13 +4,13 @@ from discord.commands import slash_command
 
 
 class Base(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: discord.Bot):
         self.bot = bot
 
     @slash_command(description="hello")
-    async def hello(self, ctx):
+    async def hello(self, ctx: discord.ApplicationContext):
         await ctx.respond(f"Hey {ctx.author.mention}")
 
 
-def setup(bot):
+def setup(bot: discord.Bot):
     bot.add_cog(Base(bot))
